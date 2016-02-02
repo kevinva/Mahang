cca4f743c256eb4c91ffbef1348eb71c26ed55be
ZanderHo <freedom_0003@qq.com>
2016-02-02 11:36:16 +0800

    update shareSDK

---
+++ KevinExample/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  KevinExample
+//
+//  Created by ZanderHo on 16/2/2.
+//  Copyright © 2016年 何 峙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
---
+++ KevinExample/SettingsViewController.m
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.m
+//  KevinExample
+//
+//  Created by ZanderHo on 16/2/2.
+//  Copyright © 2016年 何 峙. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@implementation SettingsViewController
+
+@end
